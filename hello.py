9e4d1471652df4f82fab869144e87793582b369c
Alan Akra <alanakra@hotmail.fr>
2022-12-31 16:08:10 +0100

    New routes

--- hello.py
+++ hello.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('hello.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
